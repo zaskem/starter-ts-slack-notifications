d708949f11862d72d66f4094bcd8800ebdebb1c7
Matt Zaske <zaskem@mzonline.com>
2024-11-03 17:39:25 -0600

    cURL argument cleanup.

--- TSCompleteNotifier.php
+++ TSCompleteNotifier.php
@@ -53,10 +53,7 @@
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt(
-      $ch,
-      CURLOPT_HTTPHEADER,
-      array(
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         'Content-Type: application/json',
         'Content-Length: ' . strlen($data_string)
       )
